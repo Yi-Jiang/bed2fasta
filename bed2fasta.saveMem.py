1f39c375d77674dd3be4dcc5b52fae7d9f48b4b8
Yi Jiang <jiangyi3029@foxmail.com>
2020-09-24 15:36:48 +0800

    updated bed2fasta.saveMem.py modified:   bed2fasta.saveMem.py

--- bed2fasta.saveMem.py
+++ bed2fasta.saveMem.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3.5
-import sys, re, getopt, math
+import os, sys, re, getopt, math
 
 def usage():
-    print("output format: FASTA")
     print("usage: python %s -option <argument>" %sys.argv[0])
     print("     -i     <STRING>    a string like \"chrN:start-end\" or a BED file")
-    print("     -r     <STRING>    reference genome in FASTA format")
+    print("     -r     <STRING>    can be \"hg19\", \"hg38\", or a path to reference genome [hg38]")
+    print("     --bed              output BED format [FASTA]")
     print("     --upper            convert all bases to uppercase")
     print("     --mark             mark position at the end of each line")
     print("     -h/--help                      ")
@@ -15,14 +15,13 @@ if len(sys.argv) < 2:
     sys.exit(2)
 
 try:
-    opts, args = getopt.getopt( sys.argv[1:], "i:r:h", ["help", "upper", "mark", ] )
+    opts, args = getopt.getopt( sys.argv[1:], "i:r:h", ["help", "bed", "upper", "mark", ] )
 except getopt.GetoptError:
-    print("get option error!")
+    print("Get option error!")
     usage()
     sys.exit(2)
 
-# deal with the options
-ref = "/zs32/data-analysis/reflib/genomes/human_UCSC_hg19/chrAll.fasta"
+isBed = 0
 toUpper = 0
 mark = 0
 
@@ -32,14 +31,29 @@ for opt, val in opts:
         sys.exit(1)
     else:
         if opt in ( "-i", ):
-            input = val
+            infile = val
         if opt in ( "-r", ):
             ref = val
+        if opt in ( "--bed", ):
+            isBed = 1
         if opt in ( "--upper", ):
             toUpper = 1
         if opt in ( "--mark", ):
             mark = 1
-            
+
+if "ref" in dir():
+    if ref=="hg19":
+        ref = "/home/yjiang/database/refGenome/hg19/hg19.fa"
+    elif ref=="hg38":
+        ref = "/home/data/refGenome/hg38/hg38.fa"
+    else:
+        if not os.path.isfile(ref):
+            print("Get option error: reference genome is not exist!")
+            usage()
+            sys.exit(2)
+else:
+    ref = "/home/data/refGenome/hg38/hg38.fa"
+
 # function
 refFile=open(ref,"r")
 refIndex=open("%s.fai"%ref, "r")
@@ -49,10 +63,8 @@ for line in refIndex:
     line_s = line.split("\t")
     indexDict[line_s[0]] = int(line_s[2])
     length_chr[line_s[0]] = int(line_s[1])
+lineWidth = int(line_s[3])
 refIndex.close()
-
-refFile.seek(indexDict[line_s[0]])
-lineWidth = len(refFile.readline())
 
 def bed2fasta(chr,start,end):
     if end>length_chr[chr]:
@@ -70,41 +82,51 @@ def bed2fasta(chr,start,end):
     
     return mySeq
 
-# main
-if re.search(r'\w+:\d+',input):
-    input = re.sub(",","",input)
-    input = re.sub(" ","",input)
-    if re.search("-",input):
-        ch,st,en = re.split("[:-]",input)
+def printFASTA(myseq0,ch0,st0,en0):
+    if toUpper==1:
+        myseq0 = myseq0.upper()
+    length = len(myseq0)
+    lines = math.ceil(length/lineWidth)
+    print(">",ch0,":",st0,"-",en0,sep="")
+    if mark==0:
+        for i in range(lines-1):
+            print(myseq0[i*lineWidth:(i+1)*lineWidth])
+        print(myseq0[(lines-1)*lineWidth:])
     else:
-        ch,st = re.split("[:-]",input)
+        start0 = st0
+        for i in range(lines-1):
+            print(myseq0[i*lineWidth:(i+1)*lineWidth], "[%s:%s-%s]"%(ch0,start0,start0+49))
+            start0 = start0 + lineWidth
+        print(myseq0[(lines-1)*lineWidth:], "[%s:%s-%s]"%(ch0,start0,en0))
+
+def printBED(myseq0,prefix0):
+    if toUpper==1:
+        myseq0 = myseq0.upper()
+    print("%s\t%s"%(prefix0,myseq0))
+
+# main
+if re.search(r'\w+:\d+',infile):
+    infile = re.sub(",","",infile)
+    infile = re.sub(" ","",infile)
+    if re.search("-",infile):
+        ch,st,en = re.split("[:-]",infile)
+    else:
+        ch,st = re.split("[:-]",infile)
         en = st
     st = int(st)
     en = int(en)
     myseq = bed2fasta(ch,st,en)
-    if toUpper==1:
-        myseq = myseq.upper()
-    length = len(myseq)
-    lines = math.ceil(length/lineWidth)
-    print(">",ch,":",st,"-",en,sep="")
-    if mark==0:
-        for i in range(lines-1):
-            print(myseq[i*lineWidth:(i+1)*lineWidth])
-        print(myseq[(lines-1)*lineWidth:])
+    if isBed:
+        prefixCol = "%s\t%s\t%s"%(ch,st,en)
+        printBED(myseq,prefixCol)
     else:
-        start0 = st
-        for i in range(lines-1):
-            print(myseq[i*lineWidth:(i+1)*lineWidth], "[%s:%s-%s]"%(ch,start0,start0+49))
-            start0 = start0 + lineWidth
-        print(myseq[(lines-1)*lineWidth:], "[%s:%s-%s]"%(ch,start0,en))
+        printFASTA(myseq,ch,st,en)
 else:
-    try:
-        f = open(input,"r")
-    except:
-        print("You did not input a eligible string or a file name, please check your input.")
+    if not os.path.isfile(infile):
+        print("You did not infile a eligible string or a file name, please check your infile.")
         usage()
         sys.exit(2)
-
+    f = open(infile,"r")
     while 1:
         line = f.readline()
         if not line:
@@ -115,23 +137,11 @@ else:
         st = int(st)
         en = int(en)
         myseq = bed2fasta(ch,st,en)
-        if toUpper==1:
-            myseq = myseq.upper()
-        length = len(myseq)
-        lines = math.ceil(length/lineWidth)
-        #print(">",ch,":",st,"-",en,sep="")
-        print(">%s"%line)
-        if mark==0:
-            for i in range(lines-1):
-                print(myseq[i*lineWidth:(i+1)*lineWidth])
-            print(myseq[(lines-1)*lineWidth:])
+        if isBed:
+            printBED(myseq,line)
         else:
-            start0 = st
-            for i in range(lines-1):
-                print(myseq[i*5lineWidth0:(i+1)*lineWidth], "[%s:%s-%s]"%(ch,start0,start0+49))
-                start0 = start0 + lineWidth
-            print(myseq[(lines-1)*lineWidth:], "[%s:%s-%s]"%(ch,start0,en))
-            
+            printFASTA(myseq,ch,st,en)
+
 refFile.close()
 
 
